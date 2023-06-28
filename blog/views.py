1958209c3b8a05a68442ee720040650a196883ca
Heloise Santos <heloise.santos@aluno.ifsp.edu.br>
2023-06-28 11:11:18 -0300

    Heloise Django Blog

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
 def ola(request):
     return HttpResponse('Olá Django')
