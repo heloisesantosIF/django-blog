f33229bb75d760e82ada787522570f1d4ac34b2c
Heloise Santos <fabriciahelo2020@gmail.com>
2023-08-09 10:48:46 -0300

    aula 09-08

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+
 def index(request):
     return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
 def ola(request):
-    return HttpResponse('Olá Django')
+    #return HttpResponse('Olá Django')
+    return render(request, 'home.html')
