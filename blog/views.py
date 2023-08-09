e96d38b44d6717300ca77791f9fade48cada7a9e
Heloise Santos <fabriciahelo2020@gmail.com>
2023-08-09 11:23:32 -0300

    Inclui o model Post

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from blog.models import Post
 
 def index(request):
     return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
 def ola(request):
     #return HttpResponse('Olá Django')
-    return render(request, 'home.html')
+    posts = Post.objects.all()
+    context = {'posts_list': posts } 
+    return render(request, 'posts.html', context)
+    #return render(request, 'home.html')
